aa504f38bca1ee7a4f73fc6bcd823a3edaaad9c0
n0sc3tipsum <da322@ic.ac.uk>
2024-02-27 16:08:33 +0000

    Added an additional constructor to handle functions with arguments

--- include/ast_function_definition.hpp
+++ include/ast_function_definition.hpp
@@ -9,12 +9,16 @@ private:
     Node *declaration_specifiers_;
     Node *declarator_;
     Node *compound_statement_;
+    NodeList *declaration_list_;
 
 public:
     FunctionDefinition(Node *declaration_specifiers, Node *declarator, Node *compound_statement) : declaration_specifiers_(declaration_specifiers), declarator_(declarator), compound_statement_(compound_statement){};
+    FunctionDefinition(Node *declaration_specifiers, Node *declarator, Node *compound_statement, NodeList *declaration_list) : declaration_specifiers_(declaration_specifiers), declarator_(declarator), compound_statement_(compound_statement), declaration_list_(declaration_list){};
+
     ~FunctionDefinition()
     {
         delete declaration_specifiers_;
+        delete declaration_list_;
         delete declarator_;
         delete compound_statement_;
     };
