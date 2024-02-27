994818b9015414afc154099fa29339c088609744
n0sc3tipsum <da322@ic.ac.uk>
2024-02-27 16:07:37 +0000

    Added a symbol table to the context class
    - Created a symbol table representation that will hold a variable name as well as their record
    - The identifier record will contain the variable type and value
    - The usage of this is still not known and this is a rough draft of the structure that may potentially be needed
    - Also started implementing the activation record class to keep track of procedure calls

--- include/ast_context.hpp
+++ include/ast_context.hpp
@@ -1,11 +1,38 @@
 #ifndef AST_CONTEXT_HPP
 #define AST_CONTEXT_HPP
-
+#include <string>
+#include <vector>
+#include <map>
 // An object of class Context is passed between AST nodes during compilation.
 // This can be used to pass around information about what's currently being
 // compiled (e.g. function scope and variable names).
+typedef union
+{
+    std::string string_t;
+    char char_t;
+    int int_t;
+    float float_t;
+    long long_t;
+
+}identifierValue;
+
+typedef struct
+{
+    std::string type;
+    identifierValue value;
+
+}identifierRecord;
+
+class ActivationRecord
+{
+    int _size;
+    std::string _procedure_name;
+};
+
 class Context
 {
+    std::map<std::string, identifierRecord> _symbol_table;
+
     /* TODO decide what goes inside here */
 };
 
